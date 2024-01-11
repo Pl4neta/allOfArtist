38d1cbc3ba20c4dd30301b611c1df6c721d873e9
P4lmeiras <135662804+P4lmeiras@users.noreply.github.com>
2024-01-11 19:10:08 +0000

    Update allOfArtist.js

--- allOfArtist.js
+++ allOfArtist.js
@@ -1,24 +1,37 @@
 
-(function allOfArtist() { 
-    const {
+(function allOfArtist(){ 
+    const{
         CosmosAsync,
         URI
     } = Spicetify;
-    if (!(CosmosAsync && URI)) {
+    if (!(CosmosAsync && URI)){
         setTimeout(allOfArtist, 300)
         return
     }
-  
-    const buttontxt = "Create All Of ..."
 
-  
-    function shouldDisplayContextMenu(uris) {
-        if (uris.length > 1) {
+    const buttontxt = "Create All Of ..."
+    
+    /*function btnText(uris){ 
+        buttontxt = "Create All Of " + artistName.name
+    }*/
+    
+    function makePlaylist(uris){
+        const rawUri = uris[0]
+        const uri = rawUri.split(":")[2]
+        const artistName = await CosmosAsync.get(`https://api.spotify.com/v1/artists/${uri}`)
+        Spicetify.PopupModal.display({
+            title: "Content",
+            content: artistName
+        });
+    }
+    
+    function shouldDisplayContextMenu(uris){
+        if (uris.length > 1){
             return false;
         }
-        const uri = uris[0];
+        const uri = uris[0]
         const uriObj = Spicetify.URI.fromString(uri);
-        if (uriObj.type === Spicetify.URI.Type.TRACK || uriObj.type === Spicetify.URI.Type.ARTIST) {
+        if (uriObj.type === Spicetify.URI.Type.TRACK || uriObj.type === Spicetify.URI.Type.ARTIST){
             return true;
         }
         return false;
