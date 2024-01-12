bd286374b708acb8881fe827a913d8a11da30208
P4lmeiras <ricardo.m.o.morais@gmail.com>
2024-01-12 05:26:59 +0000

    modal showing the top 50 results searching by the artist name, checking individually if they are indeed from specified artist and only showing the ones that are

--- allOfArtist.js
+++ allOfArtist.js
@@ -11,8 +11,8 @@
         URI
     } = Spicetify;
     if (!(CosmosAsync && URI)){
-        setTimeout(allOfArtist, 10)
-        return
+        setTimeout(allOfArtist, 10);
+        return;
     }
 
     const buttontxt = "Create All Of ..."
@@ -21,16 +21,70 @@
         buttontxt = "Create All Of " + artistName.name
     }*/
     
-    function makePlaylist(uris){
+
+    async function getArtist(uri){
+	var artistData = {}
+	try{
+	    let artist = await CosmosAsync.get('https://api.spotify.com/v1/artists/'+uri)
+	    artistData.id = artist.id
+	    artistData.name = artist.name
+	}
+	catch{
+	    try{
+		let artist = await CosmosAsync.get('https://api.spotify.com/v1/tracks/'+uri)
+		artistData.id = artist.artists[0].id
+		artistData.name = artist.artists[0].name
+	    }
+	    catch{
+		artistData.id = artistData.name = 'ERROR'
+	    }
+	}
+	/*if (artist.error.status == '404'){
+	    artist = await CosmosAsync.get('https://api.spotify.com/v1/tracks/'+uri)
+	    artistName = artist.artists[0].name
+	}
+	else*/
+	    //artistName = artist.error.message
+	artistData.name = artistData.name.replace(/\s/g, '%20');
+	return artistData;
+	//return 'teste';
+    }
+
+    async function makePlaylist(uris){
         const rawUri = uris[0]
         const uri = rawUri.split(":")[2]
-        //const artistName = await CosmosAsync.get(`https://api.spotify.com/v1/artists/${uri}`)
-        Spicetify.PopupModal.display({
-            title: "Content",
-            content: "Teste2",
-        });
+		const artist = await getArtist(uri)
+        var offset = 0
+        if(artist.id != 'ERROR'){
+	    	const artistTracksSearchRaw = await CosmosAsync.get('https://api.spotify.com/v1/search?query=artist%3A'+artist.name+'&type=track&offset='+offset+'&limit=50')
+	    	var artistTracks = []
+	    	var artistTracksSearch = artistTracksSearchRaw.tracks.items
+	    	for (let i = 0; i < artistTracksSearch.length; i++){
+			var check = false;
+			for (let r = 0; r < artistTracksSearch[i].artists.length; r++){
+		    	if(artistTracksSearch[i].artists[r].id == artist.id)
+				check = true;
+			}
+			if(check)
+		    	artistTracks.push(artistTracksSearch[i].name);
+	    	};
+			//artistTracksSearch.foreach((track) => {
+	    	//let t = track.name
+	    	//artistTracks.push(t);
+			//});*/
+	    	Spicetify.PopupModal.display({
+			title: "Content",
+			content: artistTracks//uri+' '+artistTracksSearch[0].artists.length//artistName //artistTracks[0]+artistTracks[1]+artistTracks[2],
+	    	});
+		}
+		else{
+	    	Spicetify.PopupModal.display({
+			title: "Content",
+			content: 'Error',
+	    	});
+		}
     }
-    
+ 
     function shouldDisplayContextMenu(uris){
         if (uris.length > 1){
             return false;
